5756da108a6acf22eef949d663c80842ec50e3fa
JonathanRebello01 <rebello.jonathan@gmail.com>
2022-06-28 21:50:46 -0300

    Adiciona backgroun animado e links de contato

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-window.alert("CUIDDADO!!!")
-
-let nome=window.prompt("digite seu nome: ")
-
-let confirmacao = window.confirm("tem certeza que deseja prosseguir, "+nome+" ?")
-
-if(confirmacao!=true){
-    window.location.replace("https://www.youtube.com/watch?v=d98KxaUYyV4")
-}
